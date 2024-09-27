8fb973c9e467eafc1fe7903a0371cb19f820ba9b
Andrejs Kuidins <andrejs.kuidins@gmail.com>
2024-09-27 19:06:17 +0300

    master

--- main.go
+++ main.go
@@ -1,37 +1,3 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
-// ---------------------------------------------------------
-// EXERCISE: Refactor the game store to funcs - step #3
-//
-//  Let's continue from the previous exercise. This time,
-//  you're going to add json encoding and decoding using
-//  funcs.
-//
-//  1- Create a new command in a func that encodes the
-//     game store data to json and terminates the program.
-//     Lastly, add it to runCmd func.
-//
-//     For more information, see: "Encode" exercise from
-//     the structs section.
-//
-//        Name  : cmdSave
-//        Inputs: []game
-//        Output: bool
-//
-//  2- Refactor the load() to load the games from the
-//     `data` constant (it's in the games.go as well).
-//
-//     For more information, see: "Decode" exercise from
-//     the structs section.
-//
-// ---------------------------------------------------------
-
 package main
 
 import (
@@ -41,7 +7,7 @@ import (
 )
 
 func main() {
-	games := load()
+	packages := load()
 	byID := indexByID(games)
 
 	fmt.Printf("Inanc's game store has %d games.\n", len(games))
