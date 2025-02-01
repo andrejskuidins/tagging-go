bb7a0b8b073f6354e5675f58b404f99b7879f796
Andrejs.Kuidins <andrejs.kuidins@1nce.com>
2025-02-01 11:12:51 +0200

    master

--- request.go
+++ request.go
@@ -5,16 +5,31 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
+type HealthCheck struct {
+    Status string `json:"status"`
+}
+
 func main() {
-	res, err := http.Get("http://www.google.com/robots.txt")
+	res, err := http.Get("https://simpledebit.gocardless.io/health_check")
 	if err != nil {
 		log.Fatal(err)
 	}
 	body, err := io.ReadAll(res.Body)
+
 	res.Body.Close()
-	if res.StatusCode > 299 {
+
+	var health HealthCheck
+	if err := json.Unmarshal(body, &health); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(health.Status) // This will print "ok"
+
+	if res.StatusCode == 200 {
+		fmt.Println("SUCCESS")
+	} else if res.StatusCode > 299 {
 		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
 	}
 	if err != nil {
